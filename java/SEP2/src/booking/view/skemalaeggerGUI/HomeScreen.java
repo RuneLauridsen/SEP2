8303fd8bd86bf4d454a89cf3820f1f4cae33e96c
juli175h <jbramsen1212@gmail.com>
2023-05-15 10:27:05 +0200

    Skemalægger homescreen gui

---
+++ java/SEP2/src/booking/view/skemalaeggerGUI/HomeScreen.java
@@ -0,0 +1,29 @@
+package booking.view.skemalaeggerGUI;
+
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
+
+public class HomeScreen
+{
+  public Label lblName;
+  public TableView tvtRooms;
+  public TableColumn tcolName;
+  public TableColumn tcolType;
+  public TableColumn tcolStatus;
+  public TableColumn tcolBook;
+  public TableColumn tcolAlter;
+
+  public void AddRoomClicked(MouseEvent mouseEvent)
+  {
+  }
+
+  public void CoursesClicked(MouseEvent mouseEvent)
+  {
+  }
+
+  public void BookingsClicked(MouseEvent mouseEvent)
+  {
+  }
+}
